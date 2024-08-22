504221020a5d4bad52c4790b8bc9322a9d1af179
YongSiHun <tlgns1537@naver.com>
2024-08-23 00:02:32 +0900

    feat: 리소스 파일 복호화 기능 개발

--- src/resource_decryptor.ts
+++ src/resource_decryptor.ts
@@ -1,18 +1,60 @@
 import path from "path";
 import fs from "fs";
+import crypto from "crypto";
+import secret from "../secret.json";
 
 export class ResourceDecryptor {
   private readonly encryptedResPath: string;
-  private readonly drugrecognitionResFileName: string;
-  private readonly finishedMedicinePermissiondetailsFileName: string;
+  private readonly drugRecognitionEncryptPath: string;
+  private readonly finishedMedicinePermissiondetailsEncryptPath: string;
 
   constructor() {
     this.encryptedResPath = path.join(__dirname, `../encrypted_res`);
-    this.drugrecognitionResFileName = "drug_recognition";
-    this.finishedMedicinePermissiondetailsFileName = "finisihed_medecine_permission_details";
+
+    this.drugRecognitionEncryptPath = path.join(
+      this.encryptedResPath,
+      "/drug_recognition"
+    );
+
+    this.finishedMedicinePermissiondetailsEncryptPath = path.join(
+      this.encryptedResPath,
+      "/finisihed_medecine_permission_details"
+    );
   }
 
-  public decrypt() {
-    const encryptedResource = fs.readFileSync(this.encryptedResPath);
+  public async decrypt() {
+    const pathList = this.getPathList();
+
+    for await (const resourcePath of pathList) {
+      const decryptedData = this.decryptResourceData(resourcePath);
+
+      const decryptedFileName = path.join(
+        __dirname,
+        `../decrypted_res/${resourcePath.split("\\").pop()}.json`
+      );
+
+      fs.writeFileSync(decryptedFileName, decryptedData);
+    }
+  }
+
+  private getPathList() {
+    return [
+      this.drugRecognitionEncryptPath,
+      this.finishedMedicinePermissiondetailsEncryptPath,
+    ];
+  }
+
+  private decryptResourceData(resourcePath: string) {
+    const { aesKey, aesIv } = secret;
+
+    const decipher = crypto.createDecipheriv(
+      "aes-256-cbc",
+      Buffer.from(aesKey, "hex"),
+      Buffer.from(aesIv, "hex")
+    );
+
+    const encryptedResourceData = fs.readFileSync(resourcePath);
+
+    return decipher.update(encryptedResourceData.toString(), "base64", "utf-8");
   }
 }
